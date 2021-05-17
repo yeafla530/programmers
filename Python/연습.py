fd5b6bb4b5cd8e4c6cb802032756faf725f8b568
yeafla530 <yeafla530@naver.com>
2021-05-18 00:07:43 +0900

    210518

--- Python/연습.py
+++ Python/연습.py
@@ -1,11 +1,33 @@
-d = [0] * 100
+from collections import deque
 
-def fibo(x):
-    print('f(' + str(x) + ')')
-    if x == 1 or x == 2:
-        return 1
-    if d[x] != 0:
-        return d[x]
-    d[x] = fibo(x - 1) + fibo(x - 2)
-    return d[x]
-fibo(6)
+v, e = map(int, input().split())
+# 진입차수
+indegree = [0] * (v+1)
+graph = [[] for _ in range(v+1)]
+
+for _ in range(e):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b] += 1
+
+def topoloty_sort():
+    result = []
+    q = deque()
+
+    for i in range(1, v+1):
+        if indegree[i] == 0:
+            q.append(i)
+    
+    while q:
+        now = q.popleft()
+        result.append(now)
+
+        for i in graph[now]:
+            indegree[i] -= 1
+            if indegree[i] == 0:
+                q.append(i)
+        
+    for i in result:
+        print(i, end=' ')
+
+topology_sort()
