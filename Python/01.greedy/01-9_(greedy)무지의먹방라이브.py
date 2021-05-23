56d1b6604121a693e8cff79c82a1819e2b6d177e
yeafla530 <yeafla530@naver.com>
2021-05-23 20:38:45 +0900

    210523 | 무지의먹방라이브

--- Python/01.greedy/01-9_(greedy)무지의먹방라이브.py
+++ Python/01.greedy/01-9_(greedy)무지의먹방라이브.py
@@ -1,1 +1,27 @@
- 
+ import heapq
+
+def solution(food_times, k):
+    # 전체 음식을 먹는 시간보다 k가 크거나 같다면
+    if sum(food_times) <= k:
+        return -1
+    
+    # 시간이 작은 음식부터 빼야됨 => 우선순위큐사용
+    q = []
+    for i in range(len(food_times)):
+        heapq.heappush(q, (food_times[i], i+1))
+    
+    length = len(food_times) # 음식 개수
+    sum_value = 0 # 음식 먹기 위해 사용한 시간
+    previous = 0 # 직전에 다 먹은 음식 시간
+    # 음식을 먹기위해 사용한 시간 + (현재 가장작은 시간에서 이전시간을 뺌) * 현재 음식개수
+    while sum_value + ((q[0][0] - previous) * length) <= k:
+        # print(q)
+        now = heapq.heappop(q)[0]
+        sum_value += (now - previous) * length
+        length -= 1
+        previous = now
+        # print(now, sum_value, length, previous)
+    
+    result = sorted(q, key=lambda x: x[1]) # 음식번호 기준 정렬
+    # print(result, k, sum_value)
+    return result[(k-sum_value) % length][1]
--- Python/01.greedy/연습.py
+++ Python/01.greedy/연습.py
@@ -1,15 +1,83 @@
-n, m = map(int, input().split())
-ball_list = list(map(int, input().split()))
+# 인접 행렬
+# 2차원 배열에 각 노드가 연결된 형태를 기록하는 방식
+# 연결되어있지 않은 노드끼리는 무한으로 표시
 
-arr = [0] * 11
-result = 0
-for x in ball_list:
-    arr[x] += 1
+INF = 987654321
 
-for i in range(1, m+1):
-    # n = b의 경우의 수
-    n -= arr[i]
-    result += arr[i] * n 
+graph = [
+    [0, 7, 5],
+    [7, 0, INF],
+    [5, INF, 0]
+]
 
-print(result)
+print(graph)
 
+# 인접리스트
+# 연결 리스트 자료 구조 이용해서 구현하는데 
+#파이썬은 리스트 자료형이 append()와 메소드 제공하므로 
+#인접리스트를 이용해 그래프 표현할 떄 단순히 2차원 리스트 사용가능
+graph = [[] for _ in range(3)]
+
+graph[0].append((1, 7))
+graph[0].append((2, 5))
+
+graph[1].append((0, 7))
+graph[2].append((0, 5))
+
+print(graph)
+
+# 인접행렬을 사용해야 속도가 빠름
+
+# DFS
+
+def dfs(graph, v, visited):
+    # 시작점 방문
+    visited[v] = True
+    print(v, end=' ')
+
+    # 시작점과 연결된 노드들을 하나하나 살펴본다
+    for i in graph[v]:
+        # 방문한 곳이 아니면 또 연결된 것들 확인하면서 재귀
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+# BFS
+from collections import deque
+
+def bfs(graph, start, visited):
+    # 시작점을 삽입
+    q = deque([start])
+    # 방문 표시
+    visited[start] = True
+
+    while q:
+        v = q.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            # 방문한 곳이 아니면
+            if not visited[i]:
+                # 담고 방문처리 바로 
+                q.append(i)
+                visited[i] = True
+
+
+
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7], 
+    [1, 4, 5],
+    [3, 5],
+    [3, 4], 
+    [7], 
+    [2, 6, 8],
+    [1, 7]
+
+]
+
+visited = [False] * 9
+visited2 = [False] * 9
+
+dfs(graph, 1, visited)
+bfs(graph, 1, visited2)
